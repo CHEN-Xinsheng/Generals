66f3a513c9d68099f6b50dfb0b166c04f94fa69c
陈鑫圣 <2021010760@secoder.net>
2023-05-21 21:16:26 +0800

    update: added 'Pixel_Controller' module framwork

---
+++ src/Pixel_Controller.sv
@@ -0,0 +1,78 @@
+module Pixel_Controller
+#(parameter WIDTH = 0, HSIZE = 0, HFP = 0, HSP = 0, HMAX = 0, VSIZE = 0, VFP = 0, VSP = 0, VMAX = 0, HSPP = 0, VSPP = 0)
+(
+    // 时钟、复位
+    input  wire clk_vga,             // vga 输入时钟 (50M)
+    input  wire reset_n,             // 上电复位信号，低有效
+
+    // 游戏逻辑生成的图像
+    input  wire [7: 0] gen_red,
+    input  wire [7: 0] gen_green,
+    input  wire [7: 0] gen_blue,
+    input  wire        use_gen,      // 当前横纵坐标对应的像素是使用游戏逻辑生成的图像(1)还是背景图(0)
+
+    // 当前横纵坐标
+    output wire [WIDTH - 1: 0] hdata_o,
+    output wire [WIDTH - 1: 0] vdata_o,
+
+    // HDMI 图像输出
+    // '_O' 后缀表示该输出将直接接到 mod_top 的对应输出
+    output wire [7: 0] video_red_O,   // 红色像素，8位
+    output wire [7: 0] video_green_O, // 绿色像素，8位
+    output wire [7: 0] video_blue_O,  // 蓝色像素，8位
+    output wire        video_hsync_O, // 行同步（水平同步）信号
+    output wire        video_vsync_O, // 场同步（垂直同步）信号
+    output wire        video_clk_O,   // 像素时钟输出
+    output wire        video_de_O     // 行数据有效信号，用于区分消隐区
+
+);
+
+assign video_clk_O = clk_vga;
+
+// 当前横纵坐标
+logic [WIDTH - 1: 0] hdata;
+logic [WIDTH - 1: 0] vdata;
+
+// 当前像素内容
+logic [7: 0] background_red;
+logic [7: 0] background_green;
+logic [7: 0] background_blue;
+
+
+// vga 模块
+vga #(WIDTH, HSIZE, HFP, HSP, HMAX, VSIZE, VFP, VSP, VMAX, HSPP, VSPP) vga800x600at75 (
+    // input
+    .clk          (clk_vga),
+    // output 
+    .hdata        (hdata),
+    .vdata        (vdata),
+    .hsync        (video_hsync_O),
+    .vsync        (video_vsync_O),
+    .data_enable  (video_de_O)
+);
+
+// 背景图绘制模块
+Background_Painter background_painter (
+    // input
+    .hdata        (hdata),
+    .vdata        (vdata),
+    // output
+    .video_red    (background_red),
+    .video_green  (background_green),
+    .video_blue   (background_blue)
+);
+
+// 图层选择
+always_comb begin
+    if (use_gen) begin
+        video_red_O   = gen_red;
+        video_green_O = gen_green;
+        video_blue_O  = gen_blue;
+    end else begin
+        video_red_O   = background_red;
+        video_green_O = background_green;
+        video_blue_O  = background_blue;
+    end
+end
+
+endmodule
--- src/background_painter.sv
+++ src/background_painter.sv
@@ -1,4 +1,4 @@
-module background_painter(
+module Background_Painter(
 	input wire clk,
 	input wire[11:0] hdata,
 	input wire[11:0] vdata,
@@ -7,7 +7,7 @@ module background_painter(
 	output wire[7:0] video_blue
 );
 
-always @(*) begin
+always_comb begin
 	if (!(hdata == 50 || hdata==100 || hdata==150 || hdata == 200 || hdata == 250 || hdata==300 || hdata==350 || hdata == 400 || hdata==450 || hdata==500 || hdata==550 || vdata == 50 || vdata==100 || vdata==150 || vdata == 200 || vdata == 250 || vdata==300 || vdata==350 || vdata == 400 || vdata==450 || vdata==500 || vdata==550) &&hdata>=50&&hdata<=550 &&vdata>=50&&vdata<=550) begin
 		video_red = 255;
 		video_green = 255;
