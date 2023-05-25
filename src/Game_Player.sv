23a58a0abe910f66dd70d6093f6c5bea258bccb7
陈鑫圣 <cxs21@mails.tsinghua.edu.cn>
2023-05-25 20:45:30 +0800

    test: use 7-segment to test 'Game_Player'

--- src/Game_Player.sv
+++ src/Game_Player.sv
@@ -1,5 +1,14 @@
 module Game_Player
-#(parameter VGA_WIDTH = 0, BORAD_WIDTH = 10, LOG2_BORAD_WIDTH = 4, LOG2_PLAYER_CNT = 3, LOG2_MAX_TROOP = 9, LOG2_MAX_ROUND = 12) (
+#(parameter VGA_WIDTH = 0, BORAD_WIDTH = 10, LOG2_BORAD_WIDTH = 4, LOG2_PLAYER_CNT = 3, LOG2_PIECE_TYPE_CNT = 2, LOG2_MAX_TROOP = 9, LOG2_MAX_ROUND = 12) (
+    //// [TEST BEGIN] 将游戏内部数据输出用于测试，以 '_o_test' 作为后缀
+    output wire [LOG2_BORAD_WIDTH - 1: 0]   cursor_h_o_test,         // 当前光标位置的横坐标（h 坐标）
+    output wire [LOG2_BORAD_WIDTH - 1: 0]   cursor_v_o_test,         // 当前光标位置的纵坐标（v 坐标）
+    output wire [LOG2_MAX_TROOP - 1: 0]     troop_o_test,            // 当前格兵力
+    output wire [LOG2_PLAYER_CNT - 1:0]     owner_o_test,            // 当前格归属方
+    output wire [LOG2_PIECE_TYPE_CNT - 1:0] piece_type_o_test,       // 当前格棋子类型
+    output wire [LOG2_PLAYER_CNT - 1:0]     current_player_o_test,   // 当前回合玩家，正常情况下应与当前格归属方一致
+    //// [TEST END]
+
     //// input
     input wire                    clock,
     input wire                    reset,
@@ -23,49 +32,65 @@ module Game_Player
 );
 
 //// [游戏内部数据 BEGIN]
-typedef enum logic[LOG2_PLAYER_CNT - 1:0] {NPC, RED, BLUE}                           player_t;   // 玩家类型
-typedef enum logic[1:0]  {TERRITORY,               MOUNTAIN,    CROWN,   CITY      } cell_t;     // 每个格子类型
-                    //    普通领地（含空白格），     山，         王城，    塔（城市）
+// 玩家类型
+typedef enum logic [LOG2_PLAYER_CNT - 1:0]        {NPC, RED, BLUE} Player;
+// 每个棋子类型
+typedef enum logic [LOG2_PIECE_TYPE_CNT - 1:0]    {TERRITORY,           MOUNTAIN,    CROWN,   CITY      } Piece;
+                                                // 普通领地（含空白格）， 山，         王城，    塔（城市）
+// 单元格结构体
 typedef struct {
-    player_t                      owner;        // 该格子归属方
-    cell_t                        cell_type;    // 该格子类型
+    Player                        owner;        // 该格子归属方
+    Piece                         piece_type;   // 该棋子类型
     reg [LOG2_MAX_TROOP - 1: 0]   troop;        // 该格子兵力值
 } Cell;
-typedef enum logic[2:0]  {W, A, S, D, SPACE, Z} operation_t;       // 键盘操作类型
+// 平面坐标结构体
+typedef struct {
+    logic [LOG2_BORAD_WIDTH - 1: 0]  h;         // 位置的横坐标（h 坐标）
+    logic [LOG2_BORAD_WIDTH - 1: 0]  v;         // 位置的纵坐标（v 坐标）
+} Position;
+// 键盘操作类型
+typedef enum logic[2:0]  {W, A, S, D, SPACE, Z, NONE} Operation;
 //   - W：000
 //   - A：001
 //   - S：010
 //   - D：011
 //   - 空格：100
 //   - Z：101
-
-Cell cells [BORAD_WIDTH - 1: 0][BORAD_WIDTH - 1: 0];    // 棋盘结构体数组
-
-operation_t                         operation;          // 最新一次操作
-logic                               op_todo;            // 最近一次操作是否尚未结算：是(1)，否(0)
-player_t                            current_player;     // 当前玩家
-logic    [LOG2_BORAD_WIDTH - 1: 0]  cursor_h;           // 当前光标位置的横坐标
-logic    [LOG2_BORAD_WIDTH - 1: 0]  cursor_v;           // 当前光标位置的纵坐标
-logic    [1: 0]                     cursor_type;        // 光标所处模式：选择模式(0x)，行棋模式(1x)
-logic    [LOG2_MAX_ROUND: 0]        round;              // 当前回合（从 1 开始）
+//   - NONE: 110 表示没有操作
 
 
+// 游戏数据
+Cell cells [BORAD_WIDTH - 1: 0][BORAD_WIDTH - 1: 0];  // 棋盘结构体数组
+
+Operation                         operation;          // 最新一次操作。 operation == NONE 表示最近一次操作已被结算，否则尚未结算
+Player                            current_player;     // 当前玩家
+Position                          cursor;             // 当前光标位置
+logic    [1: 0]                   cursor_type;        // 光标所处模式：选择模式(0x)，行棋模式(1x)
+logic    [LOG2_MAX_ROUND: 0]      round;              // 当前回合（从 1 开始）
+
+// 游戏数据初始化
 initial begin
     for (int i = 0; i < BORAD_WIDTH; i++) begin
         for (int j = 0; j < BORAD_WIDTH; j++) begin
-            // 每个格子都初始化为 RED 玩家的 CITY 类型，兵力 43
-            cells[i][j] = '{RED, CITY, 'd43};
-            // cells[i][j].owner     = RED;
-            // cells[i][j].cell_type = CITY;
-            // cells[i][j].troop     = 'd43;
+            // 每个格子都初始化为 RED 玩家的 CITY 类型，兵力 0x43
+            cells[i][j] = '{RED, CITY, 'h43};
         end
     end
-    current_player = player_t'(0);      // 初始回合玩家
-    cursor_h       = 'd3;
-    cursor_v       = 'd4;
+    operation      = NONE;              // 初始时，操作队列置空
+    current_player = Player'(1);        // 初始回合玩家
+    cursor         = '{'d0, 'd7};
     cursor_type    = 'd0;
     round          = 'd1;               // 初始回合（从 1 开始）
 end
+
+// [TEST BEGIN] 将游戏内部数据输出用于测试，以 '_o_test' 作为后缀
+assign cursor_h_o_test       = cursor.h;                                   // 当前光标位置的横坐标（h 坐标）
+assign cursor_v_o_test       = cursor.v;                                   // 当前光标位置的纵坐标（v 坐标）
+assign troop_o_test          = cells[cursor.h][cursor.v].troop;            // 当前格兵力
+assign owner_o_test          = cells[cursor.h][cursor.v].owner;            // 当前格归属方
+assign piece_type_o_test     = cells[cursor.h][cursor.v].piece_type;       // 当前格棋子类型
+assign current_player_o_test = current_player;                             // 当前回合玩家，正常情况下应与当前格归属方一致
+// [TEST END]
 
 //// [游戏内部数据 END]
 
@@ -75,26 +100,34 @@ always_ff @ (posedge clock) begin
     // 如果键盘输入模块有新数据 
     if (keyboard_ready) begin
         // 缓存一次未结算的操作
-        operation <= operation_t'(keyboard_data);
-        op_todo   <= 'b1;
+        if (keyboard_data <= 'b101) begin
+            operation <= Operation'(keyboard_data);
+        end
         // 并给键盘处理模块返回读取已完成的信号
         keyboard_read_fin <= 'b1;
     end else begin
         keyboard_read_fin <= 'b0;
     end
 end
-
 //// [与输入模块交互部分 END]
-always_ff @ (posedge clock) begin
-    // 结算一次操作
-    if (op_todo) begin
 
-        op_todo <= 'b0;  // 标记当前已经没有尚未结算的操作
-    end
-end
 
 //// [游戏逻辑部分 BEGIN]
+always_ff @ (posedge clock) begin
+    // 如果当前有尚未结算的操作，那么结算一次操作
+    if (operation != NONE) begin
+        casez (operation)
+            : 
+            // 判断操作是否合法
+            default: 
+        endcase
 
+        // 操作结算完成，将光标移动到下一回合玩家的王城
+        
+        // 标记当前已无尚未结算的操作
+        operation <= NONE;
+    end
+end
 
 
 //// [游戏逻辑部分 END]
